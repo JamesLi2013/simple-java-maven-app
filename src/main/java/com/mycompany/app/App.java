c35fad3c76e0925813f1f109b31fcb794530ef74
James Li <james.li2013168@gmail.com>
2019-01-18 16:21:43 +0800

    sdfa

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! 2019/01/18 15:14 \n github hook test11";
+    private final String message = "Hello World! 2019/01/18 16:24 \n github hook test11";
 
     public App() {}
 
