25e89c062f570ae6ac3790bce846f98ecd631bbe
James Li <james.li2013168@gmail.com>
2019-01-18 14:15:00 +0800

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! 2019/01/18 13:43 \n github hook test";
+    private final String message = "Hello World! 2019/01/18 14:14 \n github hook test";
 
     public App() {}
 
